113ce8275741bc08b2a794191c1fb7064d95a885
luxiaomin41 <2592780154@qq.com>
2020-06-28 12:25:11 +0800

    初始化项目

---
+++ app/src/main/java/luxiaomin41/com/BaseActivity.java
@@ -0,0 +1,11 @@
+package luxiaomin41.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    void initView(){};
+
+    void initData(){};
+
+}
